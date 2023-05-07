1d0e9d71152e6bda580e508fb0b25bf06ce8014b
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-05-07 14:46:35 +0200

    added new data for my app

---
+++ summer-toy-app/src/data/Produktdata.js
@@ -0,0 +1,107 @@
+
+
+const data = [
+		{
+			action: 'add-product',
+			name: 'Badring Flamingo',
+			description: 'En lekfull och färgglad badring i form av en flamingo, tillverkad av slitstarkt PVC-material. Med en diameter på 90 cm och en höjd på 70 cm, är den perfekt för sommarens strandutflykter.',
+			picture: 'https://img.freepik.com/free-vector/flamingo-swim-tube-toy-swimming-pool-summer-sea-concept_1262-13466.jpg?t=st=1683020465~exp=1683021065~hmac=90cfca784ddef962e5797b73ffd820a9ef3b953cc6bfa26e17d7793c71fe3fdd',
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Badring Anka',
+			description: "En charmig badring formad som en gullig liten anka, tillverkad av högkvalitativt vinylmaterial. Med en diameter på 80 cm och en höjd på 60 cm, blir den här badringen en favorit bland barnen.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Badring - Regnbågsfärgad',
+			description: "En iögonfallande badring med härliga regnbågsfärger, tillverkad av slitstarkt vinylmaterial. Med en diameter på 91 cm och en höjd på 30 cm, blir den här badringen en färgglad och uppskattad accessoar i vattnet.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Uppblåsbar Basketset',
+			description: "Ett roligt och uppblåsbart basketset som gör att du kan spela basket i vattnet. Tillverkat av hållbart PVC-material och med en korg som är 120 cm hög, blir detta set en garanterad hit på sommarens strandhäng.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Undervattens Ringar',
+			description: "Dyk djupt med dessa färgglada undervattens ringar, tillverkade av säkert och hållbart plastmaterial. Perfekt för att öva dykfärdigheter. Måttet på ringarna är 40 cm i diameter",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Hink set',
+			description: "Ett klassiskt hinkset för barn, inklusive en hink, spade, och kratta, tillverkat av hållbar plast. Perfekt för att bygga sandslott och gräva skatter vid stranden.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Hink och Vattenkanna set',
+			description: "Detta 10-delars sandset inkluderar en genomskinlig hink, vattenkanna, spade, kratta och flera sandformar. Tillverkat av slitstark plast, är det perfekt för kreativt strand- och sandlådelek.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: '3 st spader med en kratta',
+			description: "Detta set innehåller tre olika spadar och en kratta, alla tillverkade av tålig plast. Perfekt för grävarbete i trädgården, sandlådan eller vid stranden.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Kratta med 2 st spadar',
+			description: "Detta praktiska set består av en kratta och en spade, båda tillverkade av hållbar plast. Perfekt för användning i trädgården, sandlådan eller vid stranden.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Strandlek set',
+			description: "Detta omfattande strandlekset inkluderar en hink, spade, kratta, sandformar och mycket mer. Tillverkat av slitstark plast, kommer det att ge timmar av kreativ lek vid stranden.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Strandhink med spade och formar',
+			description: "Detta strandset inkluderar en hink, spade och flera sandformar, alla tillverkade av tålig plast. Perfekt för att bygga sandslott och utforska kreativiteten vid stranden.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+		{
+			action: 'add-product',
+			name: 'Strandhink med spade och formar',
+			description: "Detta strandset inkluderar en hink, spade och flera sandformar, alla tillverkade av tålig plast. Perfekt för att bygga sandslott och utforska kreativiteten vid stranden.",
+			picture: ,
+			price: 250,
+			shopid: shopId
+		},
+	
+
+	
+	
+]
+
+
+
