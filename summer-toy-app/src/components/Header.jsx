c2dec62fc7688e42eb2d8c047719983c0fb60ea2
AnsoMalm <annsophie.malmqvist85@hotmail.com>
2023-05-08 08:26:54 +0200

    change files

--- summer-toy-app/src/components/Header.jsx
+++ summer-toy-app/src/components/Header.jsx
@@ -2,15 +2,6 @@ import { useState } from "react"
 import HamburgerMenu from "./HambugerMenu"
 
 const Header = () => {
-	const [showHamburgerMenu, setShowHamburgerMenu] = useState(false);
-	
-	function showNav() {
-		if (!showHamburgerMenu) {
-			setShowHamburgerMenu(true); 
-		} else {
-			setShowHamburgerMenu(false);
-		}
-	}
 
 	return (
 		<header>
